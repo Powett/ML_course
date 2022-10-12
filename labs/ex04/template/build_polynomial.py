611b215db58453a9126fe820c0908274ae45d681
Nathan PELUSO <nathan.peluso@epfl.ch>
2022-10-12 11:56:29 +0200

    W4 course + lab

--- labs/ex04/template/build_polynomial.py
+++ labs/ex04/template/build_polynomial.py
@@ -17,11 +17,9 @@ def build_poly(x, degree):
     >>> build_poly(np.array([0.0, 1.5]), 2)
     array([[1.  , 0.  , 0.  ],
            [1.  , 1.5 , 2.25]])
-    """    
-    # ***************************************************
-    # COPY YOUR CODE FROM EX03 HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+    """
+    N=x.shape[0]
+    answ=np.ones((degree+1,N))
+    for i in range(1,degree+1):
+        answ[i,]=answ[i-1,]*x
+    return answ.T
--- labs/ex04/template/costs.py
+++ labs/ex04/template/costs.py
@@ -1,22 +1,35 @@
 # -*- coding: utf-8 -*-
-"""A function to compute the cost."""
+"""a function used to compute the loss."""
 
 import numpy as np
 
-def compute_mse(y, tx, w):
-    """compute the loss by mse.
-    Args:
-        y: numpy array of shape (N,), N is the number of samples.
-        tx: numpy array of shape (N,D), D is the number of features.
-        w: weights, numpy array of shape(D,), D is the number of features.
-    
-    Returns:
-        mse: scalar corresponding to the mse with factor (1 / 2 n) in front of the sum
+def compute_loss(y,tx,w):
+    return compute_loss_mse(y,tx,w)
 
-    >>> compute_mse(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), np.array([0.03947092, 0.00319628]))
-    0.006417022764962313
+
+def compute_loss_mae(y, tx, w):
+    """Calculate the loss using either MSE or MAE.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
     """
-    
-    e = y - tx.dot(w)
-    mse = e.dot(e) / (2 * len(e))
-    return mse
+    return np.sum(np.abs(y-tx@w))/y.shape[0]
+
+def compute_loss_mse(y, tx, w):
+    """Calculate the loss using either MSE or MAE.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    e=y-tx.dot(w)
+    return 1/(2*y.shape[0]) * e.dot(e)
